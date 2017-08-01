c86efba7c60cf49ad17d84954b9420c34437c897
Rodolpho Couto <rodolpho.sbaraglini@gmail.com>
2017-08-01 20:53:55 -0300

    Configures H2, Flyway and Exposed

--- src/main/kotlin/org/kotlin/springboot/Application.kt
+++ src/main/kotlin/org/kotlin/springboot/Application.kt
@@ -1,10 +1,17 @@
 package org.kotlin.springboot
 
+import org.jetbrains.exposed.spring.SpringTransactionManager
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import javax.sql.DataSource
 
 @SpringBootApplication
-class Application
+class Application {
+
+    @Bean
+    fun transactionManager(dataSource: DataSource) = SpringTransactionManager(dataSource)
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
